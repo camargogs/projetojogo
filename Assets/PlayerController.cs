11389fe538dc3f1c1da445f9c6d74b12ac3a457e
Camargo <gomes.camargo22@gmail.com>
2018-11-01 23:34:10 -0300

    adding the attack mecanic

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float jumpforce;
+	public Rigidbody2D fruta;
 
 	private Rigidbody2D rb;
 	private bool facingRight = true;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour {
 	private Animator anim; 
 	private bool noChao = false;
 	private Transform groundCheck;	
+	private int collectedItens = 0;	
 
 	// Use this for initialization
 	void Start () {
@@ -29,11 +31,16 @@ public class PlayerController : MonoBehaviour {
 			jump = true; 
 			anim.SetTrigger("Pulou");
 		}
+
+		if(Input.GetKeyUp(KeyCode.H)){
+			Attack();
+		}
+		
 	}
 	void FixedUpdate(){
 		float h = Input.GetAxisRaw("Horizontal");
 		anim.SetFloat("Velocidade", Mathf.Abs(h));
-
+		Debug.Log(h);
 		rb.velocity = new Vector2( h * speed , rb.velocity.y);
 	
 		if(h > 0 && !facingRight){
@@ -46,16 +53,47 @@ public class PlayerController : MonoBehaviour {
 			rb.AddForce(new Vector2(0, jumpforce));
 			jump = false;
 		}
+		if(Input.GetKeyDown(KeyCode.V)){
+			Run();
+		}
+		if(Input.GetKeyUp(KeyCode.V)){
+			NormalRun();
+		}
 	}
 	void fall(){
 		
 		// anim.SetTrigger("Morreu");
 	}
+
+	void Attack(){
+		Rigidbody2D clone;
+		clone = Instantiate(fruta, transform.position, transform.rotation );
+		clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+	}
+
 	void flip(){
 		facingRight = !facingRight;
 
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;  
+	}
+
+	void CollectItem(GameObject gameObject)	{
+		Debug.Log("Item coletado! total de itens:"+collectedItens);
+		this.collectedItens++;
+		Destroy(gameObject);	
+	}
+
+	void Run(){
+		speed = speed * 2;
+	}
+	void NormalRun(){
+		speed = speed / 2;
+	}
+	void OnCollisionEnter2D (Collision2D col){		
+		if(col.gameObject.tag == "collectable"){
+			CollectItem(col.gameObject);
+		}
 	}
 }
